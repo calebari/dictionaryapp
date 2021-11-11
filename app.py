3b7e8b45b007a768533c999157754b17e31b8d7a
calebari <94087696+calebari@users.noreply.github.com>
2021-11-10 19:17:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import PyDictionary
 
-st.title("Calelb's super duper dictionary app")
+st.title("Caleb's super duper dictionary app")
 dc = PyDictionary.PyDictionary()
 a = input('enter a word')
 w1 = dc.meaning(a)
