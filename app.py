56a91ad2fc8153157afeb8b9f225df6c96980165
calebari <94087696+calebari@users.noreply.github.com>
2021-11-10 19:25:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import PyDictionary
 
 st.title("Caleb's super duper dictionary app")
 dc = PyDictionary.PyDictionary()
+
+st.sidebar.title('enter your word here')
+a=st.sidebar.text_input('',30)
+
 a = input('enter a word')
 w1 = dc.meaning(a)
 print(w1)
